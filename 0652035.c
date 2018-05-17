151cd88bcf9baf26e6c69502993716b22bf16f8e
YeeWay0129 <mark01290122@gmail.com>
2018-05-18 03:38:27 +0800

    035

--- 0652035.c
+++ 0652035.c
@@ -4,10 +4,10 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a,b; 
-	scanf("%d",&a);
-	scanf("%d",&b);
-	a=a+b;
-	printf("%d",a);
-	return 0;
+	int a,b; /*«Å§ia,b*/
+	scanf("%d",&a);/*enter a*/ 
+	scanf("%d",&b);/*enter b*/
+	a=a+b;/*comput a=a+b*/
+	printf("%d",a);/*print answer*/
+	return 0;/*end*/
 }
